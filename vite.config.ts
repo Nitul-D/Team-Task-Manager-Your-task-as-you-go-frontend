73e41e34abb5f4c44d7fea8ba5fe6d63ffbcb334
Nitul Chandra Dutta <Nituldutta543@gmail.com>
2026-05-07 15:51:22 +0530

    Initial changes3

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3000,
+    host: "0.0.0.0",
+    port: 5173,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 4173,
+
+    allowedHosts: [
+      "team-task-manager-your-task-as-you-go-frontend-production.up.railway.app",
+    ],
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
