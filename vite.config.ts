06b2b58305eee2f5f139284548038173aac7d1a1
Nitul Chandra Dutta <Nituldutta543@gmail.com>
2026-05-09 14:01:43 +0530

    Adding Initial 3

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "0.0.0.0",
-    port: 5000,
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: Number(process.env.PORT) || 3000,
-
-    allowedHosts: true,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
